6fdcd775ad3882c63be8ebd7cb8180a6f7fb984b
akshay21022003 <akshayvilawadekar21@gmail.com>
2023-12-11 21:31:22 +0530

    lets get started with DOM

---
+++ lets get started with dom/demo.js
@@ -0,0 +1,23 @@
+// console.dir(document)
+// console.log(document.URL)
+// console.log(document.title)
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all) 
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+
+var x = document.getElementById('header-title')
+var header = document.getElementById('main-header')
+// console.log(x);
+// x.textContent= 'hello';
+// x.innerHTML= 'GoodBye';
+// console.log(x.textContent)
+// console.log(x.innerText) // pays attention to style as i add display none for 123 in css
+
+header.style.borderBottom = 'solid 3px #000'
+
