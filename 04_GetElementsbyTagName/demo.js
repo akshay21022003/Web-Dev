6804c6afb51cdb9c7aeea466a10c2daa395b0395
akshay21022003 <akshayvilawadekar21@gmail.com>
2023-12-12 16:59:46 +0530

    query selector all added

--- 04_GetElementsbyTagName/demo.js
+++ 04_GetElementsbyTagName/demo.js
@@ -10,21 +10,34 @@
 
 //QUERYSELECTOR//
 
-let header = document.querySelector('#main-header');
-header.style.borderBottom = "solid 4px #ccc"
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = "solid 4px #ccc"
 
-var input = document.querySelector('input');
-input.value = "Hello World"
+// var input = document.querySelector('input');
+// input.value = "Hello World"
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value="SEND"
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
 
-var item = document.querySelector('.list-group-item');
-item.style.color='red';
+// var item = document.querySelector('.list-group-item');
+// item.style.color='red';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = "blue"
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = "blue"
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = "coral"
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = "coral"
 
+// QUERY SELECTOR ALL //
+
+let titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = "hello";
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+let even = document.querySelectorAll('li:nth-child(even)');
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'red';
+    even[i].style.backgroundColor = "#ccc";
+}
