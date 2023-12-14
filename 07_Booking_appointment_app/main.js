fed9d9108205179937210cde7f43c92d1358608d
akshay21022003 <akshayvilawadekar21@gmail.com>
2023-12-14 23:54:02 +0530

    delete functionality added

--- 07_Booking_appointment_app/main.js
+++ 07_Booking_appointment_app/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('my-form');
+const userList = document.getElementById('users');
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -18,6 +19,48 @@ function handleSubmit(event) {
   document.getElementById('name').value = '';
   document.getElementById('email').value = '';
 
+  displayUsers(); 
 }
 
 form.addEventListener('submit', handleSubmit);
+
+function displayUsers() {
+  userList.innerHTML = '';
+
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+
+  users.forEach((user, index) => {
+    const li = document.createElement('li');
+    li.textContent = `Name: ${user.name}, Email: ${user.email}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteUser(index);
+    });
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', () => {
+      editUser(index);
+    });
+
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+
+    userList.appendChild(li);
+  });
+}
+
+function deleteUser(index) {
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+  
+  users.splice(index, 1);
+  
+  localStorage.setItem('users', JSON.stringify(users));
+  
+  displayUsers();
+}
+
+
+displayUsers();
